7b09fdab124296b55f6bbcad0815565ccf512ed0
mike <mike@alezan>
2012-07-20 12:05:50 +0200

    Initial setup of django structure and model

---
+++ webqcm/associate/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Etudiant(models.Model):
+	nom = models.CharField(max_length=100)
+	prenom = models.CharField(max_length=100)
+	email = models.EmailField()
+
+class Copie(models.Model):
+	num = models.IntegerField()
+	image_nom = models.FilePathField(path="/home/mike/django/images")	
+
+class Associate(models.Model):
+	copie = models.ForeignKey(Copie)
+	etudiant = models.ForeignKey(Etudiant)
